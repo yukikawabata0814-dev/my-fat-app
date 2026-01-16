5babbd6a846b4c4d6a26410d88a10798e8aae894
Yuki <yuki.kawabata0814@gmail.com>
2026-01-17 01:48:21 +0900

    Remove unused React import to fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode> ← これを消す
     <App />
+  // </React.StrictMode>, ← これも消す
 )
